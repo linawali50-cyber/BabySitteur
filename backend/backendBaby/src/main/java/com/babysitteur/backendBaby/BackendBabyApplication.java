b8dc01c9e17318ee302d08bd4c9f29518755b5f8
Lina Ouali <linawali50@gmail.com>
2026-03-24 19:58:55 +0100

    mon message

---
+++ backend/backendBaby/src/main/java/com/babysitteur/backendBaby/BackendBabyApplication.java
@@ -0,0 +1,13 @@
+package com.babysitteur.backendBaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBabyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBabyApplication.class, args);
+	}
+
+}
---
+++ backend/backendBaby/src/test/java/com/babysitteur/backendBaby/BackendBabyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babysitteur.backendBaby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBabyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
